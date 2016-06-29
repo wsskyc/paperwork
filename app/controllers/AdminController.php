7b237b6dc2296b1207be4e19f5d282d20ad65c80
Liongold <beimaginativeegroup@gmail.com>
2016-06-29 15:51:11 +0000

    Removing folders

--- app/controllers/AdminController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class AdminController extends BaseController {
-	public function showConsole() {
-		$users = User::all();
-	    return View::make('admin/console')->with('users', $users);
-	}
-}
--- app/js/paperwork/view/view.controller.js
+++
@@ -1,6 +0,0 @@
-angular.module('paperworkNotes').controller('ViewController',
-  function($scope, $rootScope, $location, $routeParams, NotesService) {
-    $scope.isVisible = function() {
-      return !$rootScope.expandedNoteLayout;
-    }
-  });
