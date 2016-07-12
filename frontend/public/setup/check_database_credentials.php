d3e1df3230cb29a7b77d44a838d58232f487c4dc
Liongold <beimaginativeegroup@gmail.com>
2016-07-12 13:17:51 +0000

    Multiple fixes to database credential check

--- frontend/public/setup/check_database_credentials.php
+++ frontend/public/setup/check_database_credentials.php
@@ -3,15 +3,17 @@
     if(!file_exists("../../app/storage/config/database.json")) {
 
         try {
-            if($_GET['driver'] === "mysql") {
-                $connection = new PDO("mysql:host=".$_GET['host'].";port=".$_GET['port'].";dbname=".$_GET['database'], $_GET['username'], $_GET['password']);
-            }else if($_GET['driver'] === 'pgsql') {
-                $connection = new PDO("pgsql:host=".$_GET['host'].";port=".$_GET['port'].";dbname=".$_GET['database'].";user=".$_GET['username'].";password=".$_GET['password']);
-            }else if($_GET['driver'] === 'sqlite') {
+            if($_POST['driver'] === "mysql") {
+                $connection = new PDO("mysql:host=".$_POST['host'].";port=".$_POST['port'].";dbname=".$_POST['database'], $_POST['username'], $_POST['password']);
+            }else if($_POST['driver'] === 'pgsql') {
+                $connection = new PDO("pgsql:host=".$_POST['host'].";port=".$_POST['port'].";dbname=".$_POST['database'].";user=".$_POST['username'].";password=".$_POST['password']);
+            }else if($_POST['driver'] === 'sqlite') {
                 $connection = new PDO("sqlite::memory:");
-            }else if($_GET['driver'] === 'sqlsrv') {
-                $connection = new PDO("sqlsrv:Server=".$_GET['host'].",".$_GET['port'].";Database=".$_GET['database'], $_GET['username'], $_GET['password']);
+            }else if($_POST['driver'] === 'sqlsrv') {
+                $connection = new PDO("sqlsrv:Server=".$_POST['host'].",".$_POST['port'].";Database=".$_POST['database'], $_POST['username'], $_POST['password']);
             }
+
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             $string = array(
                 'driver' => $_POST['driver'],
@@ -22,13 +24,12 @@
                 'port' => $_POST['port']
             );
 
-            $array = array();
             file_put_contents("../../app/storage/config/database.json", json_encode($string));
 
             try{
                 $test = $connection->query("SELECT * FROM migrations LIMIT 1");
-            }catch(PDOException $error) {
-                exec("cd ../../ && php artisan migrate --force", $array);
+            }catch(Exception $error) {
+                exec("cd ../../ && php artisan migrate --force");
             }
 
             header("Location: ".$_SERVER['HTTP_REFERRER'], true, 200);
